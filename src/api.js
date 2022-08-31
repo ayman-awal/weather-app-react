9e1dfbf348ebd0e30899093052b136e9674025ad
ayman-awal <ayman.awal36@gmail.com>
2022-08-30 21:06:47 -0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '28176a3232msh513506b8a212cc8p1d13b8jsn89c586f33ec0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d7fa96645928812f18278944d8d4e5c0"; 
